7c8c496295a01147861f0a973c1c7369eedd8998
Suhaas Narayan <suhaasn3@siebl-0224-36.ews.illinois.edu>
2021-11-26 00:17:21 -0600

    Finished loadRoutes

--- fileio.cpp
+++ fileio.cpp
@@ -17,8 +17,8 @@ namespace FileIO {
                 string name = line[1];
                 string city = line[2];
                 string country = line[3];
-                double latitude = std::stod(line[4]);
-                double longitude = std::stod(line[5]);
+                double latitude = std::stod(line[6]);
+                double longitude = std::stod(line[7]);
                 
                 Airport* a = new Airport(id, name, city, country, latitude, longitude);
                 airports.push_back(a);
@@ -28,10 +28,52 @@ namespace FileIO {
         return airports; 
     }
 
-    vector<Route*> loadRoutes(const std::string& route_filename) {
+    vector<Route*> loadRoutes(const std::string& route_filename, const std::string& airport_filename) {
         /* Might have to overload extraction operator somewhere else (maybe create a dataset class?)
         to map the airport to its attributes and then create the routes
          */
+        vector<Route*> routes;
+        ifstream airport_file(airport_filename);
+        Dataset set;
+        airport_file >> set;
+        vector<vector<string>> data = set.GetData();
+
+        ifstream my_file(route_filename);
+        vector<string> line;
+        if (my_file.isOpen()) {
+            while (getline(my_file, line, "\n")) {
+                for (const auto& vec : data) {
+                    if (line[2] == vec[4]) {
+                        unsigned src_id = std::stoul(line[0]);
+                        string src_name = vec[1];
+                        string src_city = vec[2];
+                        string src_country = vec[3];
+                        double src_latitude = std::stod(vec[6]);
+                        double src_longitude = std::stod(vec[7]);
+
+                        Airport* source = new Airport(src_id, src_name, src_city,
+                                                      src_country, src_latitude, src_longitude);
+
+                        for (const auto& vec : data) {
+                            if (line[4] == vec[4]) {
+                                unsigned dest_id = std::stoul(line[0]);
+                                string dest_name = vec[1];
+                                string dest_city = vec[2];
+                                string dest_country = vec[3];
+                                double dest_latitude = std::stod(vec[6]);
+                                double dest_longitude = std::stod(vec[7]);
+
+                                Airport* destination = new Airport(dest_id, dest_name, dest_city,
+                                                                   dest_country, dest_latitude, dest_longitude);
+                                Route* r = new Route(source, destination);
+                                routes.push_back(r);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return routes;
     }
     
 }
--- fileio.h
+++ fileio.h
@@ -3,9 +3,10 @@
 #include <vector>
 #include "airport.h"
 #include "route.h"
+#include "dataset.h"
 
 namespace FileIO {
     std::vector<Airport*>* loadAirports(const std::string& airport_filename);
     
-    std::vector<Route*>* loadRoutes(const std::string& route_filename);
+    std::vector<Route*>* loadRoutes(const std::string& route_filename, const std::string& airport_filename);
 }
